4a5964f215c6aca8c7d666ce56a766beb112d52c
Leandro Pinto <147439587+LeandroPinto52@users.noreply.github.com>
2023-10-21 23:35:58 +0000

    ejercicios hechos hasta el punto H

---
+++ entrega/guia_1/punto1.py
@@ -0,0 +1,41 @@
+"""a. Escribir un programa que muestre en la terminal
+“Las máquinas me sorprenden con mucha frecuencia.” """
+
+print("Las máquinas me sorprenden con mucha frecuencia.")
+
+#b. Mostrar una línea vacía.
+
+print("")
+
+#c. Mostrar tus propios mensajes.
+
+print("Hola me llamo leandro")
+
+#d. Mostrar el número “23” y 23, ¿Qué sucedió?
+
+print("23", 23)
+
+"""A primera vista no parece haber un cambio,
+pero el "23" es un texto, mientras que 23 sin comillas es un valor entero"""
+
+"""e. Escribir un programa que muestre el siguiente texto “Una computadora puede
+ser llamada "inteligente" si logra engañar a una persona haciéndole creer que
+es un humano.” Las comillas que encierran a la palabra inteligente deben
+imprimirse."""
+
+print('Una computadora puede ser llamada "inteligente" si logra engañar a una persona haciéndole creer que es un humano.')
+
+"""f. Escribir un programa que muestre tu nombre, apellido y tu edad como
+argumentos separados."""
+
+print('Leandro', 'Pinto', '19')
+
+"""g. Modificar el programa anterior para que en vez de que los argumentos estén
+separados por espacios, se unan por un guión bajo. Tip usar sep."""
+
+print('Leandro', 'Pinto', '19', sep='_')
+
+"""h. Escribir un programa que muestre tres argumentos: calle, número y código
+postal. Deben estar separados por una tabulación. Tip \t"""
+
+print('San Martin', '500', '9400', sep='\t')
