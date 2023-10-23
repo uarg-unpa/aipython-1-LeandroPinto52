3048207bf434f6c9d4d7a379d62c90861eebc8bf
Leandro Pinto <147439587+LeandroPinto52@users.noreply.github.com>
2023-10-23 19:03:55 +0000

    operaciones y punto 1 hasta ejercicio i

--- entrega/guia_1/punto1.py
+++ entrega/guia_1/punto1.py
@@ -5,7 +5,7 @@ print("Las máquinas me sorprenden con mucha frecuencia.")
 
 #b. Mostrar una línea vacía.
 
-print("")
+print("") #print()
 
 #c. Mostrar tus propios mensajes.
 
@@ -39,3 +39,27 @@ print('Leandro', 'Pinto', '19', sep='_')
 postal. Deben estar separados por una tabulación. Tip \t"""
 
 print('San Martin', '500', '9400', sep='\t')
+
+"""i. Modificar el ejercicio anterior para que muestre cada uno de los argumentos
+en una nueva línea."""
+
+print('San Martin', '500', '9400', sep='\n')
+
+"""k. Escribir un programa que imprima con un print “Solo podemos ver poco del
+futuro” y usando otro print “pero lo suficiente para darnos cuenta de que hay
+mucho que hacer”. La salida debe ser en una sola línea y la primera frase debe
+terminar con una ‘,’."""
+
+print("Solo podemos ver poco del futuro", end=',')
+print(" pero lo suficiente para darnos cuenta de que hay mucho que hacer")
+
+"""i. Dibujar el siguiente arbol"""
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
